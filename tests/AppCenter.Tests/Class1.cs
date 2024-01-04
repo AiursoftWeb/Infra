f904f7a6459d7a478e74c3b13ca1522dd1bb03f0
AnduinXue <anduin@aiursoft.com>
2024-01-04 13:14:30 +0000

    Add dummy tests.

---
+++ tests/AppCenter.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppCenter.Tests;
+
+[TestClass]
+public class BasicTests
+{
+    [TestMethod]
+    public void DummyTest()
+    {
+        
+    }
+}
